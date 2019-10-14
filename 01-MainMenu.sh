85ec86e046d6bc074e41b82cfb6b5df65b9fe82a
JamesRobb03 <j.a.m.robb@dundee.ac.uk>
2019-10-14 20:34:46 +0100

    started work on editfile

--- 01-MainMenu.sh
+++ 01-MainMenu.sh
@@ -1,78 +1,14 @@
 userName=$(whoami)
-notFound=true
-echo "Hello "$userName
-ls
-read -p "Please enter which repository you would like to view:" repoName
-
-while true; do
-
-		if [ -d "$repoName" ] 
-		then
-			cd $repoName;
-            ls
-			break
-		elif [ ! -d "$repoName" ] 
-		then
-			echo "Could not find please try again"
-			read -p "Please enter which repository you would like to view:" repoName
-		fi
-done
-
-echo "Menu Test, Please Select:"
-PS3='Please enter your choice: '
-options=("New File" "Edit File" "Backup & Restore" "Quit")
-select opt in "${options[@]}"
-do
-    case $opt in
-        "New File")
-			echo " "
-            echo "Please insert new file name: "
-            echo " "
-            createFile
-            ;;
-        "Edit File")
-			echo " "
-            echo "Which file would you like to edit: "
-            echo " "
-            # read fileToEditName
-            editFile
-            ;;
-        "Backup & Restore")
-			echo " "
-            echo "1) Backup"
-            echo "2) Restore"
-            echo "3) Archive"
-            echo " "
-            read varname
-            while [[ "$varname" !=  "1" && "$varname" != "2" && "$varname" != "3" ]]; do
-            	echo "This is an invalid choice. Please choose from select 1, 2, or 3 as your choice, corresponding with the above options."
-            	read varname
-            done
-
-            if [ "$varname" == "1" ] ; then
-            	backupFile
-            elif [ "$varname" == "2" ]; then
-            	restoreFile
-            
-            elif [ "$varname" == "3" ]; then 
-            	archiveFile
-
-            fi		
-            ;;
-        "Quit")
-            break
-            ;;
-        *) echo "invalid option $REPLY";;
-    esac
-done 
-
-
 #Andrew
 createFile()
 {
 	#CALL CREATEFILE BASH SCRIPT 
+    echo ""
 }
 
+#Notes for meeting:
+#menu should give option to move into folders
+#as in repo's there could be folders
 #James
 editFile()
 {
@@ -86,19 +22,106 @@ editFile()
     #create a log file and save it in folder called logs.
     #then create an automatic backup
 	#CALL EDIT FILE BASH SCRIPT
+
+    ls
+    echo " "
+    echo "Which file would you like to edit: "
+    echo " "
+    read filename
+    if [ -f "$filename" ]; then
+        mkdir $userName && mv $filename $userName && cd $userName && nano $fileName && echo "Enter Y when finished with file:" 
+        read finish
+        if [ "$finish" == "Y" ] ; then
+            echo "DONE"
+        fi
+
+
+    else
+        echo "does not exist. please try again"
+    fi
 }
 #Dillon
 backupFile()
 {
 	#CALL BACKUP FILE BASH SCRIPT
+    echo ""
 }
 #Andrew
 restoreFile()
 {
 	#CALL RESTORE FILE BASH SCRIPT
+    echo""
 }
 #Dillon
 archiveFile()
 {
 	#CALL ARCHIVE FILE BASH SCRIPT
+    echo ""
 }
+
+
+notFound=true
+
+echo "Hello "$userName
+ls
+read -p "Please enter which repository you would like to view:" repoName
+
+while true; do
+
+        if [ -d "$repoName" ] 
+        then
+            cd $repoName;
+            ls
+            break
+        elif [ ! -d "$repoName" ] 
+        then
+            echo "Could not find please try again"
+            read -p "Please enter which repository you would like to view:" repoName
+        fi
+done
+
+echo "Menu Test, Please Select:"
+PS3='Please enter your choice: '
+options=("New File" "Edit File" "Backup & Restore" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "New File")
+            echo " "
+            echo "Please insert new file name: "
+            echo " "
+            createFile
+            ;;
+        "Edit File")
+            
+            # read fileToEditName
+            editFile
+            ;;
+        "Backup & Restore")
+            echo " "
+            echo "1) Backup"
+            echo "2) Restore"
+            echo "3) Archive"
+            echo " "
+            read varname
+            while [[ "$varname" !=  "1" && "$varname" != "2" && "$varname" != "3" ]]; do
+                echo "This is an invalid choice. Please choose from select 1, 2, or 3 as your choice, corresponding with the above options."
+                read varname
+            done
+
+            if [ "$varname" == "1" ] ; then
+                backupFile
+            elif [ "$varname" == "2" ]; then
+                restoreFile
+            
+            elif [ "$varname" == "3" ]; then 
+                archiveFile
+
+            fi      
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done 
